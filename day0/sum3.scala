9a568f42e285a09f980aa58818a4fd8926928b06
shigemk2 <i.am.shige@gmail.com>
2015-07-26 20:31:45 +0900

    Monoid 2

---
+++ day0/sum3.scala
@@ -0,0 +1,13 @@
+trait Monoid[A] {
+  def mappend(a1: A, a2: A): A
+  def mzero: A
+}
+
+object IntMonoid extends Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a + b
+  def mzero: Int = 0
+}
+
+def sum(xs: List[Int], m: Monoid[Int]): Int = xs.foldLeft(m.mzero)(m.mappend)
+
+println(sum(List(1,2,3,4), IntMonoid))
