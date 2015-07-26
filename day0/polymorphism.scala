f2fa8ab991d2bfcf17d8a83cca91ba4e805539bb
shigemk2 <i.am.shige@gmail.com>
2015-07-26 19:10:35 +0900

    アドホック多相

--- day0/polymorphism.scala
+++ day0/polymorphism.scala
@@ -11,4 +11,8 @@ trait Plus[A] {
 }
 def plus[A <: Plus[A]](a1: A, a2: A): A = a1.plus(a2)
 
-
+// アドホック多相
+trait Plus[A] {
+  def plus(a1: A, a2: A): A
+}
+def plus[A: Plus](a1: A, a2: A): A = implicitly[Plus[A]].plus(a1, a2)
