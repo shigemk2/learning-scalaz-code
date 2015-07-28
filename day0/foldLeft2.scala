0744bd2400d605eab4c3c59a21b452f617aefe09
shigemk2 <i.am.shige@gmail.com>
2015-07-28 16:13:21 +0900

    foldleft

---
+++ day0/foldLeft2.scala
@@ -0,0 +1,42 @@
+trait Monoid[A] {
+  def mappend(a1: A, a2: A): A
+  def mzero: A
+}
+
+object IntMonoid extends Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a + b
+  def mzero: Int = 0
+}
+
+object StringMonoid extends Monoid[String] {
+  def mappend(a: String, b: String): String = a + b
+  def mzero: String = ""
+}
+
+implicit val intMonoid = IntMonoid
+
+implicit val stringMonoid = StringMonoid
+
+implicit val multiMonoid: Monoid[Int] = new Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a * b
+  def mzero: Int = 1
+}
+
+trait FoldLeft[F[_]] {
+  def foldLeft[A, B](xs: F[A], b: B, f: (B, A) => B): B
+}
+
+object FoldLeft {
+  implicit val FoldLeftList: FoldLeft[List] = new FoldLeft[List] {
+    def foldLeft[A, B](xs: List[A], b: B, f: (B, A) => B) = xs.foldLeft(b)(f)
+  }
+}
+
+def sum[M[_]: FoldLeft, A: Monoid](xs: M[A]): A = {
+  val m = implicitly[Monoid[A]]
+  val fl = implicitly[FoldLeft[M]]
+  fl.foldLeft(xs, m.mzero, m.mappend)
+}
+
+println(sum(List(1,2,3,4)))
+println(sum(List("a", "b", "c")))
