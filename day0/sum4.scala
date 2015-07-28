82c925b8a3840b5a6d32298ac2cca105a204d46d
shigemk2 <i.am.shige@gmail.com>
2015-07-28 14:33:22 +0900

    sum multiMonoid

---
+++ day0/sum4.scala
@@ -0,0 +1,22 @@
+trait Monoid[A] {
+  def mappend(a1: A, a2: A): A
+  def mzero: A
+}
+
+object IntMonoid extends Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a + b
+  def mzero: Int = 0
+}
+
+def sum[A](xs: List[A])(implicit m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
+
+implicit val intMonoid = IntMonoid
+
+implicit val multiMonoid: Monoid[Int] = new Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a * b
+  def mzero: Int = 1
+}
+
+println(sum(List(1,2,3,4))(IntMonoid))
+println(sum(List(1,2,3,4))(multiMonoid))
+println(sum(List(1,2,3,4)))
