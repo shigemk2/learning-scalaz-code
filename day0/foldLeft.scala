a5ff897ca8bd2415fdede273f251049044ef4501
shigemk2 <i.am.shige@gmail.com>
2015-07-28 14:56:57 +0900

    FoldLeft

--- day0/foldLeft.scala
+++ day0/foldLeft.scala
@@ -8,7 +8,14 @@ object IntMonoid extends Monoid[Int] {
   def mzero: Int = 0
 }
 
+object StringMonoid extends Monoid[String] {
+  def mappend(a: String, b: String): String = a + b
+  def mzero: String = ""
+}
+
 implicit val intMonoid = IntMonoid
+
+implicit val stringMonoid = StringMonoid
 
 implicit val multiMonoid: Monoid[Int] = new Monoid[Int] {
   def mappend(a: Int, b: Int): Int = a * b
@@ -25,5 +32,5 @@ def sum[A: Monoid](xs: List[A]): A = {
 }
 
 println(sum(List(1,2,3,4)))
-// println(sum(List("a","b","c")))
+println(sum(List("a", "b", "c"))(stringMonoid))
 println(sum(List(1,2,3,4))(multiMonoid))
