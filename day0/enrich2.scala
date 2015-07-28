132915a80f77029a6ba4c4fadaec85105ea71425
shigemk2 <i.am.shige@gmail.com>
2015-07-28 16:28:03 +0900

    enrich2

---
+++ day0/enrich2.scala
@@ -0,0 +1,28 @@
+trait Monoid[A] {
+  def mappend(a1: A, a2: A): A
+  def mzero: A
+}
+
+object IntMonoid extends Monoid[Int] {
+  def mappend(a: Int, b: Int): Int = a + b
+  def mzero: Int = 0
+}
+
+object StringMonoid extends Monoid[String] {
+  def mappend(a: String, b: String): String = a + b
+  def mzero: String = ""
+}
+
+trait MonoidOp[A] {
+  val F: Monoid[A]
+  val value: A
+  def |+|(a2: A) = F.mappend(value, a2)
+}
+
+implicit def toMonoidOp[A: Monoid](a: A): MonoidOp[A] = new MonoidOp[A] {
+  val F = implicitly[Monoid[A]]
+  val value = a
+}
+
+println(3 |+| 4)
+println("a" |+| "b")
