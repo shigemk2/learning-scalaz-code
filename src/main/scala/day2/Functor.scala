8583db0b4291950790cc99fcf99c02af771d4125
shigemk2 <i.am.shige@gmail.com>
2015-07-30 15:49:42 +0900

    Functorの関数

--- src/main/scala/day2/Functor.scala
+++ src/main/scala/day2/Functor.scala
@@ -7,7 +7,19 @@ object Functor {
     import Scalaz._
 
     println(List(1,2,3) map { _ + 1 })
-
     println((1,2,3) map { _ + 1 }) // (1,2,4)
+
+    val res1 = ((x: Int) => x + 1) map { _ * 7 }
+    println(res1(3))
+
+    val res2 = scalaz.Functor[List].lift {(_: Int) * 2}
+    println(res2(List(3)))
+
+    println(List(1,2,3) >| "x")
+    println(List(1,2,3) as "x")
+    println(List(1,2,3).fpair)
+    println(List(1,2,3).strengthL("x"))
+    println(List(1,2,3).strengthR("x"))
+    println(List(1,2,3).void)
   }
 }
