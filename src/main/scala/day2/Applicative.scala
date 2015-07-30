19755b97ee1cbfa1be00a4f6f35b35766a908d58
shigemk2 <i.am.shige@gmail.com>
2015-07-30 22:06:44 +0900

    Applicative 一旦point

--- src/main/scala/day2/Applicative.scala
+++ src/main/scala/day2/Applicative.scala
@@ -1,10 +1,14 @@
 object Applicative {
   def main(args: Array[String]): Unit = {
     import scalaz._
+    import Scalaz._
 
     // これはエラー
     // println(List(1,2,3,4) map {(_: Int) * (_: Int)})
     val res1 = List(1,2,3,4) map {(_: Int) * (_: Int)}.curried
     println(res1 map {_(9)})
+
+    // おまじない import Scalaz._ がないと死ぬ
+    println(1.point[List])
   }
 }
