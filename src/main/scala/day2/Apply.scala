915da4c0dfc87839f2c2892a05d13546157ae1e1
shigemk2 <i.am.shige@gmail.com>
2015-08-01 01:52:56 +0900

    ApplyとしてのOption

--- src/main/scala/day2/Apply.scala
+++ src/main/scala/day2/Apply.scala
@@ -8,5 +8,8 @@ object Apply {
     println(none <* 2.some)
     println(1.some *> 2.some)
     println(none *> 2.some)
+
+    println(9.some <*> {(_: Int) + 3}.some)
+    println(3.some <*> { 9.some <*> {(_: Int) + (_: Int)}.curried.some })
   }
 }
