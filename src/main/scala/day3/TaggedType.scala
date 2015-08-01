9d841da100b6c0e1f2f3f5ae7483f8a1a354a07f
shigemk2 <i.am.shige@gmail.com>
2015-08-01 16:25:02 +0900

    KiloGram

--- src/main/scala/day3/TaggedType.scala
+++ src/main/scala/day3/TaggedType.scala
@@ -1,5 +1,15 @@
-object TaggedType {
-  def main(args: Array[String]): Unit = {
+sealed trait KiloGram
 
+object TaggedType {
+  import scalaz._
+  import Scalaz._
+
+  def KiloGram[A](a: A): A @@ KiloGram = Tag[A, KiloGram](a)
+
+  def main(args: Array[String]): Unit = {
+    val mass = KiloGram(20.0)
+    println(2 * Tag.unwrap(mass))
+    println(2 * Tag.unwrap(mass))
+    println(2 * scalaz.Tag.unsubst[Double, Id, KiloGram](mass))
   }
 }
