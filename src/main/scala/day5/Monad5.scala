09e77a83005a17433d087bf25350dbd67c67936a
shigemk2 <i.am.shige@gmail.com>
2015-08-02 02:05:39 +0900

    Monad

--- src/main/scala/day5/Monad5.scala
+++ src/main/scala/day5/Monad5.scala
@@ -3,10 +3,16 @@ import Scalaz._
 
 object Monad5 {
   def main(args: Array[String]): Unit = {
+    // bind
     println(3.some flatMap { x => (x + 1).some })
     println((none: Option[Int]) flatMap { x => (x + 1).some })
 
     println(3.some flatMap { x => (x + 1).some })
     println((none: Option[Int]) flatMap { x => (x + 1).some })
+
+    // monad
+    println(Monad[Option].point("WHAT"))
+    println(9.some flatMap { x => Monad[Option].point(x * 10) })
+    println((none: Option[Int]) flatMap { x => Monad[Option].point(x * 10) })
   }
 }
