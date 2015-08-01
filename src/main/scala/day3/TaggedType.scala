bcfa8d5187f3fce817360fdc6a8caa54e9b280c3
shigemk2 <i.am.shige@gmail.com>
2015-08-01 22:27:41 +0900

    Tagged Type

--- src/main/scala/day3/TaggedType.scala
+++ src/main/scala/day3/TaggedType.scala
@@ -1,4 +1,5 @@
 sealed trait KiloGram
+sealed trait JoulePerKiloGram
 
 object TaggedType {
   import scalaz._
@@ -6,10 +7,18 @@ object TaggedType {
 
   def KiloGram[A](a: A): A @@ KiloGram = Tag[A, KiloGram](a)
 
+  def JoulePerKiloGram[A](a: A): A @@ JoulePerKiloGram = Tag[A, JoulePerKiloGram](a)
+
+  def energyR(m: Double @@ KiloGram): Double @@ JoulePerKiloGram =
+    JoulePerKiloGram(299792458.0 * 299792458.0 * Tag.unsubst[Double, Id, KiloGram](m))
+
   def main(args: Array[String]): Unit = {
     val mass = KiloGram(20.0)
     println(2 * Tag.unwrap(mass))
     println(2 * Tag.unwrap(mass))
     println(2 * scalaz.Tag.unsubst[Double, Id, KiloGram](mass))
+
+    println(energyR(mass))
+    // println(energyR(10.0))
   }
 }
