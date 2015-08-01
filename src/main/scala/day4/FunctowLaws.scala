f69596b3b39f5cca52e8c7d6449b3ecd24d2314a
shigemk2 <i.am.shige@gmail.com>
2015-08-02 00:21:41 +0900

    Functor則

---
+++ src/main/scala/day4/FunctowLaws.scala
@@ -0,0 +1,9 @@
+import scalaz._
+import Scalaz._
+
+object FunctorLaws {
+  def main(args: Array[String]): Unit = {
+    println(List(1,2,3) map {identity} assert_=== List(1,2,3))
+    println((List(1, 2, 3) map {{(_: Int) * 3} map {(_: Int) + 1}}) assert_=== (List(1, 2, 3) map {(_: Int) * 3} map {(_: Int) + 1}))
+  }
+}
