ed285a5e8393c6ae5f121d24d990b907d968fe8f
shigemk2 <i.am.shige@gmail.com>
2015-08-03 17:36:37 +0900

    綱渡り

--- src/main/scala/day5/WalkTheLine.scala
+++ src/main/scala/day5/WalkTheLine.scala
@@ -6,16 +6,23 @@ object WalkTheLine {
     type Birds = Int
 
     case class Pole(left: Birds, right: Birds) {
-      def landLeft(n: Birds): Pole = copy(left = left + n)
-      def landRight(n: Birds): Pole = copy(right = right + n)
+      def landLeft(n: Birds): Option[Pole] =
+        if (math.abs((left + n) - right) < 4) copy(left = left + n).some
+        else none
+      def landRight(n: Birds): Option[Pole] =
+        if (math.abs(left - (right + n)) < 4) copy(right = right + n).some
+        else none
     }
 
     println(Pole(0,0).landLeft(2))
-    println(Pole(1,2).landLeft(1))
-    println(Pole(1,2).landLeft(-1))
+    println(Pole(0,3).landLeft(10))
 
     // chain
-    println(Pole(0,0).landLeft(1).landRight(1).landLeft(2))
-    println(Pole(0,0).landLeft(1).landRight(4).landLeft(-1).landRight(-2))
+    println(Pole(0, 0).landRight(1) flatMap {_.landLeft(2)})
+    println((none: Option[Pole]) flatMap {_.landLeft(2)})
+    println(Monad[Option].point(Pole(0, 0)) flatMap {_.landRight(2)} flatMap {_.landLeft(2)} flatMap {_.landRight(2)})
+
+    println(Monad[Option].point(Pole(0, 0)) >>= {_.landRight(2)} >>= {_.landLeft(2)} >>= {_.landRight(2)})
+    println(Monad[Option].point(Pole(0, 0)) >>= {_.landLeft(1)} >>= {_.landRight(4)} >>= {_.landLeft(-1)} >>= {_.landRight(-2)})
   }
 }
