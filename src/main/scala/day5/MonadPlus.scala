02effc562eb454f2ca34407a7d8e65fb52aeba5e
shigemk2 <i.am.shige@gmail.com>
2015-08-10 20:33:06 +0900

    MonadPlus

--- src/main/scala/day5/MonadPlus.scala
+++ src/main/scala/day5/MonadPlus.scala
@@ -9,5 +9,9 @@ object MonadPlus {
     } yield x)
     println(List(1, 2, 3) <+> List(4, 5, 6))
     println((1 |-> 50) filter { x => x.shows contains '7' })
+    println((1 |-> 50) filter { x => x.shows contains '8' })
+    // println((1 |-> 50) filter { x => x.shows contains '13' })
+    println((1 |-> 50) filter { x => x.shows contains "13" })
+    println((1 |-> 50) filter { x => x.shows contains "51" })
   }
 }
