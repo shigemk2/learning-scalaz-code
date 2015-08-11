2a341a128f4252e775ed231b25defb5d3f37934a
shigemk2 <i.am.shige@gmail.com>
2015-08-11 15:54:28 +0900

    3回のチェイン

--- src/main/scala/day5/Knights.scala
+++ src/main/scala/day5/Knights.scala
@@ -12,10 +12,16 @@ object Knights {
             KnightPos(c - 1, r - 2), KnightPos(c - 1, r + 2)) if (
             ((1 |-> 8) contains c2) && ((1 |-> 8) contains r2))
         } yield KnightPos(c2, r2)
+      def in3: List[KnightPos] =
+        for {
+          first <- move
+          second <- first.move
+          third <- second.move
+        } yield third
+      def canReachIn3(end: KnightPos): Boolean = in3 contains end
     }
 
-    println(KnightPos(6, 2).move)
-    println(KnightPos(8, 1).move)
-    println(KnightPos(1, 2).move)
+    println(KnightPos(6, 2) canReachIn3 KnightPos(6, 1))
+    println(KnightPos(6, 2) canReachIn3 KnightPos(7, 3))
   }
 }
