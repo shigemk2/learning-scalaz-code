78a40735e9198c34326758063d496e3936d46ff3
shigemk2 <i.am.shige@gmail.com>
2015-08-11 17:25:26 +0900

    右単位元

--- src/main/scala/day5/MonadLaws.scala
+++ src/main/scala/day5/MonadLaws.scala
@@ -3,7 +3,9 @@ import Scalaz._
 
 object MonadLaws {
   def main(args: Array[String]): Unit = {
+    // 左単位元
     println((Monad[Option].point(3) >>= { x => (x + 100000).some }) assert_=== 3 |> { x => (x + 100000).some })
-
+    // 右単位元
+    println(("move on up".some flatMap {Monad[Option].point(_)}) assert_=== "move on up".some)
   }
 }
