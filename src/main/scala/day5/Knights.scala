300df6481683be7e93b45e5c0af79588c344ce77
shigemk2 <i.am.shige@gmail.com>
2015-08-11 20:54:00 +0900

    Knight

--- src/main/scala/day5/Knights.scala
+++ src/main/scala/day5/Knights.scala
@@ -12,12 +12,17 @@ object Knights {
             KnightPos(c - 1, r - 2), KnightPos(c - 1, r + 2)) if (
             ((1 |-> 8) contains c2) && ((1 |-> 8) contains r2))
         } yield KnightPos(c2, r2)
-      def in3: List[KnightPos] =
-        for {
-          first <- move
-          second <- first.move
-          third <- second.move
-        } yield third
+      // def in3: List[KnightPos] =
+      //   for {
+      //     first <- move
+      //     second <- first.move
+      //     third <- second.move
+      //   } yield third
+      def in3: List[KnightPos] = for {
+        first <- move
+        second <- first.move
+        third <- second.move
+      } yield third
       def canReachIn3(end: KnightPos): Boolean = in3 contains end
     }
 
