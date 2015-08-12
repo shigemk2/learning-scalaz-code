a479fda620c03e60c6fa68f43e429660f1352771
shigemk2 <i.am.shige@gmail.com>
2015-08-12 18:16:01 +0900

    状態付き計算 Haskell

---
+++ src/main/scala/day7/stack.hs
@@ -0,0 +1,15 @@
+-- 状態付き計算
+type Stack = [Int]
+pop :: Stack -> (Int, Stack)
+pop (x:xs) = (x, xs)
+push :: Int -> Stack -> ((), Stack)
+push a xs = ((), a:xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack = let
+    ((), newStack1) = push 3 stack
+    (a , newStack2) = pop newStack1
+    in pop newStack2
+
+main = do
+    print $ stackManip [5,8,2,1]
