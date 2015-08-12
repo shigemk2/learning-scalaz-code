032527d434b27012565d4c9e29f718065b27869a
shigemk2 <i.am.shige@gmail.com>
2015-08-12 22:46:57 +0900

    状態の取得と設定

--- src/main/scala/learningscalaz/day7/StateT.scala
+++ src/main/scala/learningscalaz/day7/StateT.scala
@@ -7,13 +7,16 @@ object StateT {
   def main(args: Array[String]): Unit = {
     type Stack = List[Int]
 
-    val pop = State[Stack, Int] {
-      case x :: xs => (xs, x)
-    }
+    val pop: State[Stack, Int] = for {
+      s <- get[Stack]
+      val (x :: xs) = s
+      _ <- put(xs)
+    } yield x
 
-    def push(a: Int) = State[Stack, Unit] {
-      case xs => (a :: xs, ())
-    }
+    def push(x: Int): State[Stack, Unit] = for {
+      xs <- get[Stack]
+      r <- put(x :: xs)
+    } yield r
 
     def stackManip: State[Stack, Int] = for {
       _ <- push(3)
@@ -22,5 +25,13 @@ object StateT {
     } yield(b)
 
     println(stackManip(List(5, 8, 2, 1)))
+
+    def stackyStack: State[Stack, Unit] = for {
+      stackNow <- get
+      r <- if (stackNow === List(1, 2, 3)) put(List(8, 3, 1))
+      else put(List(9, 2, 1))
+    } yield r
+
+    println(stackyStack(List(1, 2, 3)))
   }
 }
