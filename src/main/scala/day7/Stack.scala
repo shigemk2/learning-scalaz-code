0749429c3bdb42e26f0c5bb2af3f1c0a698005c1
shigemk2 <i.am.shige@gmail.com>
2015-08-12 19:36:18 +0900

    Stack Scala

---
+++ src/main/scala/day7/Stack.scala
@@ -0,0 +1,22 @@
+import scalaz._
+import Scalaz._
+
+object Stack {
+  def main(args: Array[String]): Unit = {
+    type Stack = List[Int]
+
+    def pop(stack: Stack): (Int, Stack) = stack match {
+      case x :: xs => (x, xs)
+    }
+
+    def push(a: Int, stack: Stack): (Unit, Stack) = ((), a :: stack)
+
+    def stackManip(stack: Stack): (Int, Stack) = {
+      val (_, newStack1) = push(3, stack)
+      val (a, newStack2) = pop(newStack1)
+      pop(newStack2)
+    }
+
+    println(stackManip(List(5, 8, 2, 1)))
+  }
+}
