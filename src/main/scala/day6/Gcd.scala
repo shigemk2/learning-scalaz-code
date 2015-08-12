c92366286e87ced20003b67bc4f1eb3efa88c874
shigemk2 <i.am.shige@gmail.com>
2015-08-12 16:08:20 +0900

    マイクロベンチマーク

--- src/main/scala/day6/Gcd.scala
+++ src/main/scala/day6/Gcd.scala
@@ -22,5 +22,38 @@ object Gcd {
       } yield result
 
     println(gcdReverse(8, 3).run)
+
+    def vectorFinalCountDown(x: Int): Writer[Vector[String], Unit] = {
+      import annotation.tailrec
+      @tailrec def doFinalCountDown(x: Int, w: Writer[Vector[String], Unit]): Writer[Vector[String], Unit] = x match {
+        case 0 => w >>= { _ => Vector("0").tell }
+        case x => doFinalCountDown(x - 1, w >>= { _ =>
+          Vector(x.shows).tell
+        })
+      }
+      val t0 = System.currentTimeMillis
+      val r = doFinalCountDown(x, Vector[String]().tell)
+      val t1 = System.currentTimeMillis
+      r >>= { _ => Vector((t1 - t0).shows + " msec").tell }
+    }
+
+    def listFinalCountDown(x: Int): Writer[List[String], Unit] = {
+      import annotation.tailrec
+      @tailrec def doFinalCountDown(x: Int, w: Writer[List[String], Unit]): Writer[List[String], Unit] = x match {
+        case 0 => w >>= { _ => List("0").tell }
+        case x => doFinalCountDown(x - 1, w >>= { _ =>
+          List(x.shows).tell
+        })
+      }
+      val t0 = System.currentTimeMillis
+      val r = doFinalCountDown(x, List[String]().tell)
+      val t1 = System.currentTimeMillis
+      r >>= { _ => List((t1 - t0).shows + " msec").tell }
+    }
+
+    val a = vectorFinalCountDown(10000).run
+    println(a._1.last)
+    val b = listFinalCountDown(10000).run
+    println(b._1.last)
   }
 }
