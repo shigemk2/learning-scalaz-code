6c51817dfec4329a2bf81f0fc9937f9227d2c489
shigemk2 <i.am.shige@gmail.com>
2015-08-12 17:34:58 +0900

    Readerモナド Scala

---
+++ src/main/scala/day6/Reader.scala
@@ -0,0 +1,14 @@
+import scalaz._
+import Scalaz._
+
+object Reader {
+  // Reader モナドは値が既にあるかのようなフリをさせてくれる
+  def main(args: Array[String]): Unit = {
+    val addStuff: Int => Int = for {
+      a <- (_: Int) * 2
+      b <- (_: Int) + 10
+    } yield a + b
+
+    println(addStuff(3))
+  }
+}
