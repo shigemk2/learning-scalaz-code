cdd1bd10bbf89d97a849bec6ac0b659aceebdb61
shigemk2 <i.am.shige@gmail.com>
2015-08-12 16:05:32 +0900

    ユークリッドの互除法 逆順

--- src/main/scala/day6/Gcd.scala
+++ src/main/scala/day6/Gcd.scala
@@ -11,5 +11,16 @@ object Gcd {
         List(a.shows + " mod " + b.shows + " = " + (a % b).shows).tell >>= { _ => gcd(b, a % b) }
 
     println(gcd(8, 3).run)
+
+    def gcdReverse(a: Int, b: Int): Writer[Vector[String], Int] =
+      if (b == 0) for {
+        _ <- Vector("Finished with " + a.shows).tell
+      } yield a
+      else for {
+        result <- gcdReverse(b, a % b)
+        _ <- Vector(a.shows + " mod " + b.shows + " = " + (a % b).shows).tell
+      } yield result
+
+    println(gcdReverse(8, 3).run)
   }
 }
