03788d1cf42e1f9d8adf42e27a333987662f29a2
shigemk2 <i.am.shige@gmail.com>
2015-08-13 22:29:33 +0900

    Scala 標準ライブラリの Either 型はそれ単体ではモナドではないため、Scalaz を使っても使わなくても flatMap メソッドを実装しない

--- src/main/scala/learningscalaz/day7/Either.scala
+++ src/main/scala/learningscalaz/day7/Either.scala
@@ -7,5 +7,8 @@ object Either {
   def main(args: Array[String]): Unit = {
     println(1.right[String])
     println("error".left[Int])
+
+    // Scala 標準ライブラリの Either 型はそれ単体ではモナドではないため、Scalaz を使っても使わなくても flatMap メソッドを実装しない
+    // println(Left[String, Int]("boom") flatMap { x => Right[String, Int](x + 1) })
   }
 }
