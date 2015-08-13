955757c0c2c765b213c8afea0c4c75e6f9376a83
shigemk2 <i.am.shige@gmail.com>
2015-08-13 22:35:53 +0900

    Scalaz の \/ はだいたいにおいて右投射が欲しいだろうと決めてかかってくれる

--- src/main/scala/learningscalaz/day7/Either.scala
+++ src/main/scala/learningscalaz/day7/Either.scala
@@ -11,5 +11,14 @@ object Either {
     // Scala 標準ライブラリの Either 型はそれ単体ではモナドではないため、Scalaz を使っても使わなくても flatMap メソッドを実装しない
     // println(Left[String, Int]("boom") flatMap { x => Right[String, Int](x + 1) })
     println(Left[String, Int]("boom").right flatMap { x => Right[String, Int](x + 1) })
+
+    println("boom".left[Int] >>= { x => (x + 1).right })
+
+    println(for {
+      e1 <- "event 1 ok".right
+      e2 <- "event 2 failed!".left[String]
+      e3 <- "event 3 failed!".left[String]
+    } yield (e1 |+| e2 |+| e3)
+    )
   }
 }
