9ee3113b51da2ce0f864204bde56fc865596dd81
shigemk2 <i.am.shige@gmail.com>
2015-08-15 18:49:22 +0900

    solveRPN 改 Scala

--- src/main/scala/learningscalaz/day8/RPN.scala
+++ src/main/scala/learningscalaz/day8/RPN.scala
@@ -5,24 +5,28 @@ import Scalaz._
 
 object solveRPN {
   def main(args: Array[String]): Unit = {
-    def foldingFunction(list: List[Double], next: String): List[Double] = (list, next) match {
-      case (x :: y :: ys, "*") => (y * x) :: ys
-      case (x :: y :: ys, "+") => (y + x) :: ys
-      case (x :: y :: ys, "-") => (y - x) :: ys
-      case (xs, numString) => numString.toInt :: xs
+    def foldingFunction(list: List[Double], next: String): Option[List[Double]] = (list, next) match {
+      case (x :: y :: ys, "*") => ((y * x) :: ys).point[Option]
+      case (x :: y :: ys, "+") => ((y + x) :: ys).point[Option]
+      case (x :: y :: ys, "-") => ((y - x) :: ys).point[Option]
+      case (xs, numString) => numString.parseInt.toOption map {_ :: xs}
     }
 
     println(foldingFunction(List(3, 2), "*"))
-    // println(foldingFunction(Nil, "*"))
-    // println(foldingFunction(Nil, "wawa"))
-    // println(foldingFunction(List(3, 2), "wawa"))
+    println(foldingFunction(Nil, "*"))
+    println(foldingFunction(Nil, "wawa"))
+    println(foldingFunction(List(3, 2), "wawa"))
 
-    def solveRPN(s: String): Double =
-      (s.split(' ').toList.foldLeft(Nil: List[Double]) {foldingFunction}).head
-
+    def solveRPN(s: String): Option[Double] = for {
+      List(x) <- s.split(' ').toList.foldLeftM(Nil: List[Double]) {foldingFunction}
+    } yield x
     println(solveRPN("10 4 3 + 2 * -"))
 
     println("1".parseInt.toOption)
     println("foo".parseInt.toOption)
+
+    println(solveRPN("1 2 * 4 +"))
+    println(solveRPN("1 2 * 4"))
+    println(solveRPN("1 8 garbage"))
   }
 }
