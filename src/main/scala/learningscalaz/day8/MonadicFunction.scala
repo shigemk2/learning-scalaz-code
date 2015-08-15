aec35e406ee06e214e8780c03317936e73300fc2
shigemk2 <i.am.shige@gmail.com>
2015-08-15 17:03:32 +0900

    filterM

--- src/main/scala/learningscalaz/day8/MonadicFunction.scala
+++ src/main/scala/learningscalaz/day8/MonadicFunction.scala
@@ -10,5 +10,8 @@ object MonadicFunction {
     println(List(List(1, 2, 3), List(4, 5, 6)).join)
     println(9.right[String].right[String].join)
     println("boom".left[Int].right[String].join)
+
+    println(List(1, 2, 3) filterM { x => List(true, false) })
+    println(Vector(1, 2, 3) filterM { x => Vector(true, false) })
   }
 }
