5d31857ff3313d097eb069c48612095e9a06a035
shigemk2 <i.am.shige@gmail.com>
2015-08-15 13:19:52 +0900

    wrapNel

--- src/main/scala/learningscalaz/day7/Validation.scala
+++ src/main/scala/learningscalaz/day7/Validation.scala
@@ -7,5 +7,8 @@ object Validation {
   def main(args: Array[String]): Unit = {
     println("event 1 ok".success[String])
     println("event 1 failed!".failure[String])
+    println(("event 1 ok".success[String] |@| "event 2 failed!".failure[String] |@| "event 3 failed!".failure[String]) {_ + _ + _})
+
+    println(1.wrapNel)
   }
 }
