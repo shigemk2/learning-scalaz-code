69d8ad52476f9e87678e353f3f3ef873124cf797
shigemk2 <i.am.shige@gmail.com>
2015-08-15 16:49:07 +0900

    モナド便利関数

---
+++ src/main/scala/learningscalaz/day8/MonadicFunction.scala
@@ -0,0 +1,14 @@
+package learningscalaz.day8
+
+import scalaz._
+import Scalaz._
+
+object MonadicFunction {
+  def main(args: Array[String]): Unit = {
+    println((Some(9.some): Option[Option[Int]]).join)
+    println((Some(none): Option[Option[Int]]).join)
+    println(List(List(1, 2, 3), List(4, 5, 6)).join)
+    println(9.right[String].right[String].join)
+    println("boom".left[Int].right[String].join)
+  }
+}
