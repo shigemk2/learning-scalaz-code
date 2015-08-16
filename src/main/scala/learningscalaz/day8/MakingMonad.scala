e3bbed1bf5d1ea8f78db2f9f77561f02589d54e3
shigemk2 <i.am.shige@gmail.com>
2015-08-17 00:18:19 +0900

    モナドを作る Scala

--- src/main/scala/learningscalaz/day8/MakingMonad.scala
+++ src/main/scala/learningscalaz/day8/MakingMonad.scala
@@ -3,14 +3,19 @@ package learningscalaz.day8
 import scalaz._
 import Scalaz._
 
+case class Prob[A](list: List[(A, Double)])
+case object Prob extends ProbInstances
+
+trait ProbInstances {
+  implicit val probInstance = new Functor[Prob] {
+    def map[A, B](fa: Prob[A])(f: A => B): Prob[B] =
+      Prob(fa.list map { case (x, p) => (f(x), p) })
+  }
+  implicit def probShow[A]: Show[Prob[A]] = Show.showA
+}
+
 object MakingMonad {
   def main(args: Array[String]): Unit = {
-    case class Prob[A](list: List[A, Double])
-
-    trait ProbInstance {
-      implicit def probShow[A]: Show[Prob[A]] = Show.showA
-    }
-
-    case object Prob extends ProbInstance
+    println(Prob((3, 0.5) :: (5, 0.25) :: (9, 0.25) :: Nil) map {-_})
   }
 }
