f4096011004e8967e5e34648f20d104bf9eff9c8
shigemk2 <i.am.shige@gmail.com>
2015-08-17 11:04:25 +0900

    よくわからない

--- src/main/scala/learningscalaz/day8/MakingMonad.scala
+++ src/main/scala/learningscalaz/day8/MakingMonad.scala
@@ -3,6 +3,10 @@ package learningscalaz.day8
 import scalaz._
 import Scalaz._
 
+sealed trait Coin
+case object Heads extends Coin
+case object Tails extends Coin
+
 case class Prob[A](list: List[(A, Double)])
 case object Prob extends ProbInstances
 
@@ -18,10 +22,24 @@ trait ProbInstances {
       Prob(fa.list map { case (x, p) => (f(x), p) })
   }
   implicit def probShow[A]: Show[Prob[A]] = Show.showA
+
+  implicit val coinEqual: Equal[Coin] = Equal.equalA
+
+  def coin: Prob[Coin] = Prob(Heads -> 0.5 :: Tails -> 0.5 :: Nil)
+  def loadedCoin: Prob[Coin] = Prob(Heads -> 0.1 :: Tails -> 0.9 :: Nil)
+
+  def flipThree: Prob[Boolean] = for {
+    a <- coin
+    b <- coin
+    c <- loadedCoin
+  } yield { List(a, b, c) all {_ === Tails} }
+
 }
+
 
 object MakingMonad {
   def main(args: Array[String]): Unit = {
     println(Prob((3, 0.5) :: (5, 0.25) :: (9, 0.25) :: Nil) map {-_})
+    // println(flipThree)
   }
 }
