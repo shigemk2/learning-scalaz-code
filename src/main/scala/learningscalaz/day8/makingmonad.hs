a14d88a6cf5ec0805473ba80410a07ea399058fd
shigemk2 <i.am.shige@gmail.com>
2015-08-17 10:55:27 +0900

    Prob Haskell

--- src/main/scala/learningscalaz/day8/makingmonad.hs
+++ src/main/scala/learningscalaz/day8/makingmonad.hs
@@ -1,7 +1,13 @@
 import Data.Ratio
 
+newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
+
+instance Functor Prob where
+    fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
 main = do
     print $ 1 % 4
     print $ 1 % 2 + 1 % 2
     print $ 1 % 3 + 5 % 4
     print $ [(3,1%2),(5,1%4),(9,1%4)]
+    print $ fmap negate (Prob [(3,1%2),(5,1%4),(9,1%4)])
