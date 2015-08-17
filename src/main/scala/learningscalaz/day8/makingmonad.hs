1010f54d9b6e8728af202d6351ecc74a86af9cdb
shigemk2 <i.am.shige@gmail.com>
2015-08-17 11:06:55 +0900

    よくわからない Haskell

--- src/main/scala/learningscalaz/day8/makingmonad.hs
+++ src/main/scala/learningscalaz/day8/makingmonad.hs
@@ -1,9 +1,25 @@
 import Data.Ratio
+import Data.List (all)
 
 newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
 
 instance Functor Prob where
     fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads,1%2),(Tails,1%2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads,1%10),(Tails,9%10)]
+
+flipThree :: Prob Bool
+flipThree = do
+    a <- coin
+    b <- coin
+    c <- loadedCoin
+    return (all (==Tails) [a,b,c])
 
 main = do
     print $ 1 % 4
