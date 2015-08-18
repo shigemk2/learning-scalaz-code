6dde12dd1a3b94c3e47e6641b23f992c99033b78
shigemk2 <i.am.shige@gmail.com>
2015-08-18 18:30:21 +0900

    Id Scala

--- src/main/scala/learningscalaz/day9/IdZ.scala
+++ src/main/scala/learningscalaz/day9/IdZ.scala
@@ -7,5 +7,11 @@ object IdZ {
   def main(args: Array[String]): Unit = {
     println(1 + 2 + 3 |> {_.point[List]})
     println(1 + 2 + 3 |> {_ * 6})
+
+    println(1 visit { case x@(2|3) => List(x * 2) })
+    println(2 visit { case x@(2|3) => List(x * 2) })
+    println(1 visit { case x@(1|3) => List(x * 2) })
+    println(3 visit { case x@(1|3) => List(x * 2) })
+    println(9 visit { case x@(1|3) => List(x * 2) })
   }
 }
