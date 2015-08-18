07d0f0bafd96d9de2ca39484d39748715e8f8909
shigemk2 <i.am.shige@gmail.com>
2015-08-18 17:54:48 +0900

    Zipper 2 Scala

--- src/main/scala/learningscalaz/day9/ZipperZ.scala
+++ src/main/scala/learningscalaz/day9/ZipperZ.scala
@@ -7,5 +7,9 @@ object ZipperZ {
   def main(args: Array[String]): Unit = {
     println(Stream(1, 2, 3, 4))
     println(Stream(1, 2, 3, 4).toZipper)
+
+    println(Stream(1, 2, 3, 4).toZipper >>= {_.next})
+    println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next})
+    println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.previous})
   }
 }
