81eba93aa8ddca3aaf3bd2a9c7873cc790d9e4cd
shigemk2 <i.am.shige@gmail.com>
2015-08-18 18:39:37 +0900

    Lawless Scala

---
+++ src/main/scala/learningscalaz/day9/Lawless.scala
@@ -0,0 +1,12 @@
+package learningscalaz.day9
+
+import scalaz._
+import Scalaz._
+
+object Lawless {
+  def main(args: Array[String]): Unit = {
+    // println(List(1, 2, 3)(3))
+    println(List(1, 2, 3)(2))
+    // println(List(1, 2, 3) index 3)
+  }
+}
