f0f2de5748c298ab74e4de85786064b64c07911b
shigemk2 <i.am.shige@gmail.com>
2015-08-18 16:31:23 +0900

    Comment Out

--- src/main/scala/learningscalaz/day8/MakingMonad.scala
+++ src/main/scala/learningscalaz/day8/MakingMonad.scala
@@ -28,11 +28,11 @@ trait ProbInstances {
   def coin: Prob[Coin] = Prob(Heads -> 0.5 :: Tails -> 0.5 :: Nil)
   def loadedCoin: Prob[Coin] = Prob(Heads -> 0.1 :: Tails -> 0.9 :: Nil)
 
-  def flipThree: Prob[Boolean] = for {
-    a <- coin
-    b <- coin
-    c <- loadedCoin
-  } yield { List(a, b, c) all {_ === Tails} }
+  // def flipThree: Prob[Boolean] = for {
+  //   a <- coin
+  //   b <- coin
+  //   c <- loadedCoin
+  // } yield { List(a, b, c) all {_ === Tails} }
 
 }
 
