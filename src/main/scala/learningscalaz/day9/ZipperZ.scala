5348e82d5c918064bd1f7f1d229cee60448c10dd
shigemk2 <i.am.shige@gmail.com>
2015-08-18 18:09:19 +0900

    Zipper 4 Scala for

--- src/main/scala/learningscalaz/day9/ZipperZ.scala
+++ src/main/scala/learningscalaz/day9/ZipperZ.scala
@@ -12,6 +12,15 @@ object ZipperZ {
     println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next})
     println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.previous})
 
-    println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.modify {_ >= 7 }.some})
+    val a = Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.modify {_ => 7 }.some}
+    // val a = Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.modify {_ >= 7 }.some}
+    println(a)
+    println(a.get.toStream.toList)
+
+    println(for {
+      z <- Stream(1, 2, 3, 4).toZipper
+      n1 <- z.next
+      n2 <- n1.next
+    } yield { n2.modify {_ => 7} })
   }
 }
