4bcd506bdef4a302d4c7e8f12da7fd779d455f16
shigemk2 <i.am.shige@gmail.com>
2015-08-18 18:00:48 +0900

    Zipper 3 Stream modify toStream

--- src/main/scala/learningscalaz/day9/ZipperZ.scala
+++ src/main/scala/learningscalaz/day9/ZipperZ.scala
@@ -11,5 +11,7 @@ object ZipperZ {
     println(Stream(1, 2, 3, 4).toZipper >>= {_.next})
     println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next})
     println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.previous})
+
+    println(Stream(1, 2, 3, 4).toZipper >>= {_.next} >>= {_.next} >>= {_.modify {_ >= 7 }.some})
   }
 }
