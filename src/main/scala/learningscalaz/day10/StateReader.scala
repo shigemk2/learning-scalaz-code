86f715934f0cdd3280b71f4da4324c92ede61dca
shigemk2 <i.am.shige@gmail.com>
2015-08-19 19:02:06 +0900

    ReaderT

--- src/main/scala/learningscalaz/day10/StateReader.scala
+++ src/main/scala/learningscalaz/day10/StateReader.scala
@@ -14,5 +14,33 @@ object StateReader {
     } yield (a, b, c)
 
     println(localExample("Fred"))
+
+    type ReaderTOption[A, B] = ReaderT[Option, A, B]
+    object ReaderTOption extends KleisliInstances with KleisliFunctions {
+      def apply[A, B](f: A => Option[B]): ReaderTOption[A, B] = kleisli(f)
+    }
+
+    def configure(key: String) = ReaderTOption[Map[String, String], String] {_.get(key)}
+
+    def setupConnection = for {
+      host <- configure("host")
+      user <- configure("user")
+      password <- configure("password")
+    } yield (host, user, password)
+
+    val goodConfig = Map(
+      "host" -> "eed3si9n.com",
+      "user" -> "sa",
+      "password" -> "****"
+    )
+
+    println(setupConnection(goodConfig))
+
+    val badConfig = Map(
+      "host" -> "example.com",
+      "user" -> "sa"
+    )
+    println(setupConnection(goodConfig))
+    println(setupConnection(badConfig))
   }
 }
