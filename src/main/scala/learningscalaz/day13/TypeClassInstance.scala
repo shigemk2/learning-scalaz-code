c228981b0402f934f0ad1b292f8a906024254e57
shigemk2 <i.am.shige@gmail.com>
2015-08-26 20:55:49 +0900

    Scalazの型クラス syntax

--- src/main/scala/learningscalaz/day13/TypeClassInstance.scala
+++ src/main/scala/learningscalaz/day13/TypeClassInstance.scala
@@ -15,4 +15,19 @@ object AllInstancesSample {
   }
 }
 
+// Scalaz 型クラス syntax
+object ScalaZTypeClass {
+  import scalaz.syntax.monad._
+  import scalaz.std.option._
+  def main(args: Array[String]): Unit = {
+    println(0.point[Option])
+  }
+}
+
+object ScalaZAll {
+  import scalaz.syntax.all._
+  def main(args: Array[String]): Unit = {
+    println(1.leaf)
+  }
+}
 
