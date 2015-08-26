b694d8d401d7bdfea136446b350751b3eafb5bfd
shigemk2 <i.am.shige@gmail.com>
2015-08-26 15:00:13 +0900

    Streamの畳み込み

--- src/main/scala/learningscalaz/day12/DataListSample.scala
+++ src/main/scala/learningscalaz/day12/DataListSample.scala
@@ -6,5 +6,8 @@ import Scalaz._
 object DataListSample {
   def main(args: Array[String]): Unit = {
     println(DList.unfoldr(10, { (x: Int) => if (x == 0) none else (x, x - 1).some }).toList)
+
+    val res36 = unfold(10) { (x) => if (x == 0) none else (x, x - 1).some }
+    println(res36.toList)
   }
 }
