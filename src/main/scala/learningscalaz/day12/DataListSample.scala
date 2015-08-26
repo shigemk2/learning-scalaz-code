5ed3216bb48332baedcd1e569df96f55b19f71bd
shigemk2 <i.am.shige@gmail.com>
2015-08-26 15:04:45 +0900

    ちょっと直す

--- src/main/scala/learningscalaz/day12/DataListSample.scala
+++ src/main/scala/learningscalaz/day12/DataListSample.scala
@@ -5,9 +5,12 @@ import Scalaz._
 
 object DataListSample {
   def main(args: Array[String]): Unit = {
-    println(DList.unfoldr(10, { (x: Int) => if (x == 0) none else (x, x - 1).some }).toList)
+    val res35 = DList.unfoldr(10, { (x: Int) => if (x == 0) none else (x, x - 1).some })
+    println(res35.toList)
 
     val res36 = unfold(10) { (x) => if (x == 0) none else (x, x - 1).some }
     println(res36.toList)
+
+
   }
 }
