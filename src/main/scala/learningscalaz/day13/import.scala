35a0564b45721f2253ef33582661a65b3797eaf4
shigemk2 <i.am.shige@gmail.com>
2015-08-26 19:47:35 +0900

    syntax.ToTypeClassOps

--- src/main/scala/learningscalaz/day13/import.scala
+++ src/main/scala/learningscalaz/day13/import.scala
@@ -7,5 +7,11 @@ object Import {
   def main(args: Array[String]): Unit = {
     // import scalaz._
     println(scalaz.Monad[scalaz.Id.Id])
+    println(1.node(2.leaf))
+    // syntax.ToTypeClassOps
+    println(1.set("log1"))
+    println("log2".tell)
+    println(1.success[String])
+    println("boom".failureNel[Int])
   }
 }
