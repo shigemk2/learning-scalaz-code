809ec5b216d076f6816e217d4444df40d428bb45
shigemk2 <i.am.shige@gmail.com>
2015-08-28 18:33:43 +0900

    並列合成

--- src/main/scala/learningscalaz/day15/ScalazParallelComposition.scala
+++ src/main/scala/learningscalaz/day15/ScalazParallelComposition.scala
@@ -8,7 +8,7 @@ object ScalazParallelComposition {
     // 並列合成
     val f = { (x: Int) => x + 1}
     val g = { (x: Int) => List(x, 5) }
-    val h = f &&& g
+    val h = { (x: Int) => (f(x), g(x)) }
     println(List(1, 2, 3) traverseU f)
     println(List(1, 2, 3) traverseU g)
     println(List(1, 2, 3) traverseU h)
