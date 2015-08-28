a8d9875166a23eaf038926d54f61298a1140e882
shigemk2 <i.am.shige@gmail.com>
2015-08-28 18:32:28 +0900

    並列合成

---
+++ src/main/scala/learningscalaz/day15/ScalazParallelComposition.scala
@@ -0,0 +1,16 @@
+package learningscalaz.day15
+
+import scalaz._
+import Scalaz._
+
+object ScalazParallelComposition {
+  def main(args: Array[String]): Unit = {
+    // 並列合成
+    val f = { (x: Int) => x + 1}
+    val g = { (x: Int) => List(x, 5) }
+    val h = f &&& g
+    println(List(1, 2, 3) traverseU f)
+    println(List(1, 2, 3) traverseU g)
+    println(List(1, 2, 3) traverseU h)
+  }
+}
