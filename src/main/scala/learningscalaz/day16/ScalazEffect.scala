97f32c056bdd3d5107533f65e411cee1b90f0d66
shigemk2 <i.am.shige@gmail.com>
2015-08-29 22:51:25 +0900

    エラトステネスのふるい

--- src/main/scala/learningscalaz/day16/ScalazEffect.scala
+++ src/main/scala/learningscalaz/day16/ScalazEffect.scala
@@ -19,5 +19,35 @@ object ScalazEffect {
 
     type ForallST[A] = Forall[({type λ[S] = ST[S, A]})#λ]
     println(runST(new ForallST[Int] { def apply[S] = e2[S] }))
+
+    // エラトステネスのふるい
+    def mapM[A, S, B](xs: List[A])(f: A => ST[S, B]): ST[S, List[B]] =
+      Monad[({type λ[α] = ST[S, α]})#λ].sequence(xs map f)
+
+    def sieve[S](n: Int) = for {
+      arr <- newArr[S, Boolean](n + 1, true)
+      _ <- arr.write(0, false)
+      _ <- arr.write(1, false)
+      val nsq = (math.sqrt(n.toDouble).toInt + 1)
+      _ <- mapM (1 |-> nsq) { i =>
+        for {
+          x <- arr.read(i)
+          _ <-
+          if (x) mapM (i * i |--> (i, n)) { j => arr.write(j, false) }
+          else returnST[S, List[Boolean]] {Nil}
+        } yield ()
+      }
+      r <- arr.freeze
+    } yield r
+
+    // type ForallST[A] = Forall[({type λ[S] = ST[S, A]})#λ]
+
+    def prime(n: Int) =
+      runST(new ForallST[ImmutableArray[Boolean]] { def apply[S] = sieve[S](n) }).toArray.
+        zipWithIndex collect { case (true, x) => x }
+
+    println(prime(50).toList)
+    // Fedora20 Thinkpad X201環境だとjava.lang.ArrayIndexOutOfBoundsException
+    // println(prime(1000).toList)
   }
 }
