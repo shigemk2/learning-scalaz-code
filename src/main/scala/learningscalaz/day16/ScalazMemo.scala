4d164e6cac80f6f1a898d3ba0ba6b5438c2a3dda
shigemk2 <i.am.shige@gmail.com>
2015-08-29 22:12:59 +0900

    memo fib

--- src/main/scala/learningscalaz/day16/ScalazMemo.scala
+++ src/main/scala/learningscalaz/day16/ScalazMemo.scala
@@ -4,6 +4,11 @@ import scalaz._
 import Scalaz._
 
 object ScalazMemo {
+  val memoizedFib: Int => Int = Memo.mutableHashMapMemo {
+    case 0 => 0
+    case 1 => 1
+    case n => memoizedFib(n - 2) + memoizedFib(n - 1)
+  }
   def main(args: Array[String]): Unit = {
     // この書き方だとforward reference extends over definition of value slowFibになるので、defで関数定義する
     // val slowFib: Int => Int = {
@@ -18,6 +23,10 @@ object ScalazMemo {
     }
     println(slowFib(30))
     println(slowFib(40))
+    // println(slowFib(45))
+
+    println(memoizedFib(30))
+    println(memoizedFib(40))
     // println(slowFib(45))
   }
 }
