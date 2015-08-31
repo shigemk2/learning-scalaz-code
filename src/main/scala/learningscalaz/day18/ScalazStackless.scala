c56684f410267a808a7d727f929e5d90bc80199d
shigemk2 <i.am.shige@gmail.com>
2015-08-31 14:34:17 +0900

    Freeを用いたリスト error: object Suspend in object Free cannot be accessed in object scalaz.Free ってなるから一旦保留

--- src/main/scala/learningscalaz/day18/ScalazStackless.scala
+++ src/main/scala/learningscalaz/day18/ScalazStackless.scala
@@ -25,5 +25,18 @@ object ScalazStackless {
     println(odd(List(2,2,2)).run)
     println(odd(Nil).run)
     println(odd(0 |-> 3000).run)
+    // Free を用いたリスト
+    // error: object Suspend in object Free cannot be accessed in object scalaz.Free
+    // ってなるから一旦保留
+    type FreeMonoid[A] = Free[({type λ[+α] = (A,α)})#λ, Unit]
+    // def cons[A](a: A): FreeMonoid[A] = Free.Suspend[({type λ[+α] = (A,α)})#λ, Unit]((a, Free.Return[({type λ[+α] = (A,α)})#λ, Unit](())))
+    // println(cons(1))
+    // println(cons(1) >>= {_ => cons(2)})
+
+    def toList[A](list: FreeMonoid[A]): List[A] =
+      list.resume.fold(
+        { case (x: A, xs: FreeMonoid[A]) => x :: toList(xs) },
+        { _ => Nil })
+
   }
 }
