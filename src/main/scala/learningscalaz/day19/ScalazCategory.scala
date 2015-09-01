f20bf26212fa1e3ac02ea4ae62cf23fa3fa32d24
shigemk2 <i.am.shige@gmail.com>
2015-09-01 11:20:57 +0900

    朝食の射の例

--- src/main/scala/learningscalaz/day19/ScalazCategory.scala
+++ src/main/scala/learningscalaz/day19/ScalazCategory.scala
@@ -10,11 +10,25 @@ object ScalazCategory {
   case object John extends Person {}
   case object Mary extends Person {}
   case object Sam extends Person {}
+  // 朝食の射
+  sealed trait Breakfast {}
+  case object Eggs extends Breakfast {}
+  case object Oatmeal extends Breakfast {}
+  case object Toast extends Breakfast {}
+  case object Coffee extends Breakfast {}
+
+  val favoriteBreakfast: Person => Breakfast = {
+    case John => Eggs
+    case Mary => Coffee
+    case Sam  => Coffee
+  }
 
   def main(args: Array[String]): Unit = {
     val a: Set[Person] = Set[Person](John, Mary, Sam)
     // val a = Set[Person](John, Mary, Sam)
     // val a: List[Person] = List[Person](John, Mary, Sam)
     println(a)
+    println(favoriteBreakfast(John))
+    println(favoriteBreakfast(Mary))
   }
 }
